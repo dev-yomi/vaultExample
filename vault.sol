ab330d5c23286974278712aa21c1790902f9a5a9
yomidev_ <123413762+dev-yomi@users.noreply.github.com>
2024-02-28 13:50:30 +0000

    Create vault.sol

---
+++ vault.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+}
+
+contract StakedETHVault {
+
+    address public owner;
+    IERC20 public assetToDeposit;
+    IERC20 public assetToWithdraw;
+
+    event Deposited(address indexed user, uint256 amount);
+    event Withdrawn(address indexed to, uint256 amount);
+
+    mapping(address => uint256) public tokenDeposits;
+    mapping(address => mapping(address => uint256)) public userDeposits;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function deposit(uint256 amount, address _assetToDeposit) external {
+        assetToDeposit = IERC20(_assetToDeposit);
+        require(assetToDeposit.transferFrom(msg.sender, address(this), amount), "Transfer failed");
+        tokenDeposits[_assetToDeposit] += amount;
+        userDeposits[msg.sender][_assetToDeposit] += amount;
+        emit Deposited(msg.sender, amount);
+    }
+
+    function withdrawToOwner(uint256 amount, address _assetToWithdraw) external onlyOwner {
+        assetToWithdraw = IERC20(_assetToWithdraw);
+        require(assetToWithdraw.transfer(owner, amount), "Transfer failed");
+        tokenDeposits[_assetToWithdraw] -= amount;
+        emit Withdrawn(owner, amount);
+    }
+
+    function changeOwner(address newOwner) external onlyOwner {
+        owner = newOwner;
+    }
+}
